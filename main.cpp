bbbb041156977c0bf2697ff618d7fbffc081df15
wiwyil2tr <108447154+wiwyil2tr@users.noreply.github.com>
2023-04-08 18:39:37 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <stdio.h> //输入输出有关的函数
-#include <math.h> //数学函数 求任意两个整数之和
-int main() //函数首部
-{ float a,b,s; //定义变量
+#include <stdio.h>
+#include <math.h> 
+int main() 
+{ float a,b,s; 
 	     printf("请输入两个数\n");
-	     scanf("%f,%f",&a,&b);// 10 2 16
-	     s=a+b;//注意顺序 赋值
-             printf("s=%.2f\n",s);//s=   普通字符 原样输出
-}//函数体
+	     scanf("%f,%f",&a,&b);
+	     s=a+b;
+             printf("s=%.2f\n",s);
+}
